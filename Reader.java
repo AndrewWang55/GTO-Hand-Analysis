3e3dde35bf3a9227714a7bf874f99939d3e6a07a
Andrew Wang <ayw24@cornell.edu>
2020-01-10 22:22:02 -0500

    fix bit calcs

--- Reader.java
+++ Reader.java
@@ -17,8 +17,14 @@ class Hand {
     public Hand(int i, int j) {
         this.first_card = i;
         this.second_card = j;
+    }
+
+    public Hand() {
         this.primeMap = new HashMap<String, Integer>();
         this.handMap = new HashMap<String, Integer>();
+    }
+    
+    public void populateMap() {
 
         primeMap.put("2", 2);
         primeMap.put("3", 3);
@@ -28,7 +34,7 @@ class Hand {
         primeMap.put("7", 13);
         primeMap.put("8", 17);
         primeMap.put("9", 19);
-        primeMap.put("10", 23);
+        primeMap.put("T", 23);
         primeMap.put("J", 29);
         primeMap.put("Q", 31);
         primeMap.put("K", 37);
@@ -42,13 +48,12 @@ class Hand {
         handMap.put("7", 5);
         handMap.put("8", 6);
         handMap.put("9", 7);
-        handMap.put("10", 8);
+        handMap.put("T", 8);
         handMap.put("J", 9);
         handMap.put("Q", 10);
         handMap.put("K", 11);
         handMap.put("A", 12);
     }
-    
     /**
     [bitHandRep s] is the integer bit representation of the string representation
     [s] of the current hand. Reference Cactus Kev's hand evaluator algorithm
@@ -58,10 +63,12 @@ class Hand {
         char first_card = s.charAt(0);
         char second_card = s.charAt(2);
         
-        int primeRank1 = primeMap.get(first_card);
-        int rank1 = handMap.get(first_card);
-        int primeRank2 = primeMap.get(second_card);
-        int rank2 = handMap.get(second_card);
+        populateMap();
+
+        int primeRank1 = primeMap.get(Character.toString(first_card));
+        int rank1 = handMap.get(Character.toString(first_card));
+        int primeRank2 = primeMap.get(Character.toString(second_card));
+        int rank2 = handMap.get(Character.toString(second_card));
 
         int first, second, suitrank1, suitrank2;
         
@@ -90,10 +97,10 @@ class Hand {
             suitrank2 = 4;
         }
         
-        first = primeRank1 + (rank1 << 8) + (1 << (8 + suitrank1)) 
-                    + (1 << (8 + suitrank1 + rank1));
-        second = primeRank2 + (rank2 << 8) + (1 << (8 + suitrank2)) 
-                    + (1 << (8 + suitrank2 + rank2));
+        first = primeRank1 + (rank1 << 8) + (1 << (11 + suitrank1)) 
+                    + (1 << (12 + suitrank1 + rank1));
+        second = primeRank2 + (rank2 << 8) + (1 << (11 + suitrank2)) 
+                    + (1 << (12 + suitrank2 + rank2));
         
         return new Hand(first, second);
     } 
@@ -114,8 +121,13 @@ class Reader {
             reader = new BufferedReader(new InputStreamReader(System.in));
             // Redo later for graphics where you can highlight the hand on a GTO table and specify suit
             System.out.println("What is your hand? Denote hearts as h, spades as s, clubs as c, diamonds as d. Ex: Ah6c");
-            Hand start = new Hand(0, 0);
+            Hand start = new Hand();
             hand = start.bitHandRep(reader.readLine());
+
+            System.out.println("int first: " + hand.first_card);
+            System.out.println("binary first: " + Integer.toBinaryString(hand.first_card));
+            System.out.println("int second: " + hand.second_card);
+            System.out.println("binary second: " + Integer.toBinaryString(hand.second_card));
             
             // Click a button or something
             System.out.println("What is your position in the hand? Denote 1 as SB up to 6 as BTN");
